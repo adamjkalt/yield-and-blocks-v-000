f6f56542d2e7c92ddc6f1d862a754f38a4354af6
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-13 17:10:18 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,8 @@
-require "pry"
-
 def hello_t(array)
   if block_given?
   i = 0
     while i < array.length
+      yield (array[i])
       i = i + 1
     end
     array
@@ -13,7 +12,7 @@ end
 end
 
 # call your method here!
-binding.pry
+
 hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
